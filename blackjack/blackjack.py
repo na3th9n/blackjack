126a0c83136601854040cdda5598f4237db26112
Xie <nathan.xie@magna.com>
2024-04-09 16:12:48 -0400

    Game works

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -1,5 +1,6 @@
 import random
 import os
+import time
 
 class Blackjack:
     heart = "\u2665"
@@ -8,10 +9,10 @@ class Blackjack:
     club = "\u2663"
 
     suits = {
-        "diamonds": diamond,
-        "hearts": heart,
-        "spades": spade,
-        "clubs": club
+        "diamond": diamond,
+        "heart": heart,
+        "spade": spade,
+        "club": club
     }
 
     def __init__(self, num_decks = 1, num_players = 1, start_bal = 10):
@@ -28,7 +29,7 @@ class Blackjack:
     def create_deck(self, num_decks):
         # create a new deck if the game just started or if the cut card was hit
         suits = ["diamond", "heart", "spade", "club"]
-        ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+        ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
         deck = [{"rank":rank, "suit":suit} for _ in range(num_decks) for rank in ranks for suit in suits]
         random.shuffle(deck)
 
@@ -56,7 +57,7 @@ class Blackjack:
         result = ""
         for card in cards:
             suit = Blackjack.suits[card["suit"]]
-            result += f"{card['number']}{suit} "
+            result += f"{card['rank']}{suit}"
 
         return result.strip()
     
@@ -65,57 +66,44 @@ class Blackjack:
         if card["rank"] == "cut":
             self.cut_card = True
             card = self.deck.pop()
-
+        
         return card
 
-    def player_turn(self, player, action):
+    def player_turn(self, player_stats, action):
         if action == "h":
-            self.player["hand"].append(self.deal_card())
-            self.player["hand_val"] = self.update_hand_val(self.player["hand"])
+            player_stats["hand"].append(self.deal_card())
+            player_stats["hand_val"] = Blackjack.update_hand_val(player_stats["hand"])
+            print(Blackjack.format_cards(player_stats["hand"]))
 
         # elif action == "sp":
-
         # elif action == "dd":
 
         else:
             print("Unknown move, try again")
 
     def dealer_turn(self):
-        dealer = self.dealer
-        print(dealer["hand"])
+        print(f"Dealer has {Blackjack.format_cards(self.dealer['hand'])}")
 
-        while dealer["hand_val"] < 17:
+        while self.dealer["hand_val"] < 17: 
+            self.dealer["hand"].append(self.deal_card())
+            self.dealer["hand_val"] = Blackjack.update_hand_val(self.dealer["hand"])
+
             print("Dealer hits")
-            card = self.deck.pop()
-
-            if card["rank"] == "cut":
-                self.cut_card = True
-                card = self.deck.pop()
-
-            self.update_hand_val(dealer, card)
-            dealer["hand"].append(card)
-            print(dealer["hand"])
-
-        if dealer["hand_val"] > 21:
-            print("Dealer bust")
-        else:
-            print("Dealer stands")
-
-        return
+            print(f"Dealer:{Blackjack.format_cards(self.dealer['hand'])}, Hand Value: {self.dealer['hand_val']}")
 
     @staticmethod
-    def update_hand_val(self, hand):
+    def update_hand_val(hand):
         value = 0
         aces = 0
 
         for card in hand:
             card_val = card["rank"]
 
-            if card_val == "Ace":
+            if card_val == "A":
                 value += 11 
                 aces += 1
 
-            elif card_val in ["10", "Jack", "Queen", "King"]:
+            elif card_val in ["10", "J", "Q", "K"]:
                 value += 10
 
             else:
@@ -126,9 +114,47 @@ class Blackjack:
             aces -= 1
 
         return value
+            
+    @staticmethod
+    def place_bet(bet, player, player_stats):
+        player_stats["cur_bet"] = bet
+        player_stats["bal"] -= bet
 
-    # def insurance(self, cur_players):
-    #     for player, player_stats in cur_players:
+    @staticmethod
+    def hand_status(hand_val):
+        if hand_val > 21:
+            return True
+
+        return False
+
+    def game_results(self, player, player_stats):
+        if Blackjack.hand_status(self.dealer["hand_val"]):
+            print(f"Dealer busts, {player} wins ${2*player_stats['cur_bet']}")
+            player_stats["bal"] += 2*player_stats["cur_bet"] 
+
+        elif player_stats["hand_val"] > self.dealer["hand_val"]:
+            print(f'{player} wins ${2*player_stats["cur_bet"]}')
+            player_stats["bal"] += 2*player_stats["cur_bet"] 
+
+        elif player_stats["hand_val"] == self.dealer["hand_val"]:
+            print(f'{player} ties with dealer, player wins ${player_stats["cur_bet"]}')
+            player_stats["bal"] += player_stats["cur_bet"] 
+
+        else:
+            print(f'{player} loses ${player_stats["cur_bet"]}')
+
+    def reset_round(self):
+        for player in self.players.values():
+            player["hand"] = []
+            player["cur_bet"] = 0
+            player["hand_val"] = 0
+            player["insurance"] = False
+
+        self.dealer["hand"] = []
+        self.dealer["hand_val"] = 0
+
+    # def insurance(self, self.players):
+    #     for player, player_stats in self.players:
     #         # ask for insurance
     #         while True:
     #             ans = input(f"{player}, do you want insurance (Y/N): ")
@@ -143,7 +169,7 @@ class Blackjack:
     #                 print("Invalid response, answer again")
 
     #         if self.dealer["hand_val"] == 21:
-    #             for player, player_stats in cur_players:
+    #             for player, player_stats in self.players:
     #                 if player_stats["insurance"] != True and player_stats["hand_val"] != 21:
     #                     player_stats["bal"] -= player_stats["cur_bet"]
 
@@ -152,27 +178,21 @@ class Blackjack:
     #         else:
     #             print("No 21, game continue")
     #             return False
-            
-    def reset_round(self):
-        for player, player_stats in self.players.items():
-            player_stats["hand"] = []
-            player_stats["cur_bet"] = 0
-            player_stats["hand_val"] = 0
-            player_stats["insurance"] = False
-
-        self.dealer["hand"] = []
-        self.dealer["hand_val"] = 0
 
     def main(self):
         exit = False       
         while not exit:
+            cur_players = self.players.copy()
+            
             if self.cut_card:
                 self.shuffle_deck()
-            
-            cur_players = self.players
+
+        
+            for player_stats in self.players.values():
+                print(player_stats)
 
             # ask players for bet amounts
-            for player, player_stats in cur_players.items():
+            for player, player_stats in self.players.items():
                 while True:
                     bet = int(input(f"{player}, how much do you want to bet (min $1): "))
 
@@ -180,83 +200,65 @@ class Blackjack:
                         print("Min bet not reached or invalid bet amount, try again")
 
                     else:
-                        print(f"{player} bets ${bet}")
-                        player_stats["cur_bet"] = bet
-                        player_stats["bal"] -= bet
+                        Blackjack.place_bet(bet, player, player_stats) 
                         break
 
             # give players and dealer their cards and sum hand value
             for i in range(2):
-                for player_stats in cur_players.items():
+                for player_stats in self.players.values():
                     card = self.deal_card()
                     player_stats["hand"].append(card)
                     player_stats["hand_val"] = Blackjack.update_hand_val(player_stats["hand"])
 
-                card = self.deal_card
+                card = self.deal_card()
                 self.dealer["hand"].append(card)
                 self.dealer["hand_val"] = Blackjack.update_hand_val(self.dealer["hand"])
 
             # display players cards and dealer cards
-            for player, player_stats in cur_players.items():
+            for player, player_stats in self.players.items():
                 player_hand = Blackjack.format_cards(player_stats["hand"])
                 print(f"{player}: {player_hand}")
             
-            dealer_hand = Blackjack.format_cards(self.deal_card["hand"][0])
-            print(f"Dealer: {dealer_hand}, Unknown")   
+            dealer_hand = Blackjack.format_cards([self.dealer["hand"][0]])
+            print(f"Dealer: {dealer_hand}")   
 
             # do insurance
             # if dealer_hand[0]["rank"] == "Ace":
-            #     if self.insurance(cur_players):
+            #     if self.insurance(self.players):
             #         exit = True
             #         # add a way to end the game and reset everyones hands
 
-            cur_players_temp = cur_players.copy() # temp dictionary for modifying purposes
-            for player, player_stats in cur_players_temp.items():
+            for player, player_stats in self.players.items():
                 while True:
-                    action = input("What's your move (s/h): ")
+                    action = input(f"{player}, what's your move (s/h): ")
 
                     if action == "s":
-                        print("Stands")
+                        break
 
                     if action == "h":
-                        self.player_turn(player_stats, player_stats["hand"])
+                        self.player_turn(player_stats, action)
                         
-                        if player_stats["hand_val"] > 21:
+                        if Blackjack.hand_status(player_stats["hand_val"]):
                             print(f'{player} busts at {player_stats["hand_val"]}')
                             del cur_players[player]
                             break
                         
                         player_hand = Blackjack.format_cards(player_stats["hand"])
                         player_hand_val = player_stats["hand_val"]
-                        print("Hits")
                         print(f"{player}'s hand: {player_hand}, hand value: {player_hand_val}")
 
-            self.dealer_turn()
-
             if cur_players:
-                for player, player_stats in cur_players.items():
-                    print(f"{player}, {player_stats}")
-                    if self.dealer["hand_val"] > 21 or player_stats["hand_val"] > self.dealer["hand_val"]:
-                        print(f'{player} wins')
-                        player_stats["bal"] += 2*player_stats["cur_bet"] 
+                self.dealer_turn()
 
-                    elif player_stats["hand_val"] == self.dealer["hand_val"]:
-                        print(f'{player} ties with dealer, player wins ${player_stats["cur_bet"]}')
+                for player in cur_players:
+                    self.game_results(player, self.players[player])
+                    print(Blackjack.format_cards(self.players[player]["hand"]))
 
-                    else:
-                        print(f'{player} loses ${player_stats["cur_bet"]}')
-
-                    print(f"{player}, {player_stats}")
-
+                print(f"Dealer:{Blackjack.format_cards(self.dealer['hand'])}, Hand Value: {self.dealer['hand_val']}")
             else:
                 print("Everyone busts, round end")
-
-            self.reset_round()
-
-    def player_status(hand_val):
-        if hand_val > 21:
-
             
+            self.reset_round()
 
 if __name__ == "__main__":
     # set up game settings
